d87a3e66a34b7ed09abc2eb907ae1c5b687df8a6
Jessica Song <jiaqi.jessica.song@gmail.com>
2018-09-15 16:00:11 -0400

    Update index.js

--- index.js
+++ index.js
@@ -3,13 +3,8 @@
 
 'use strict';
 const Alexa = require('alexa-sdk');
+========================================================================================================================================
 
-//=========================================================================================================================================
-//TODO: The items below this comment need your attention.
-//=========================================================================================================================================
-
-//Replace with your app ID (OPTIONAL).  You can find this value at the top of your skill's page on http://developer.amazon.com.
-//Make sure to enclose your value in quotes, like this: const APP_ID = 'amzn1.ask.skill.bb4045e6-b3e8-4133-b650-72923c5980f1';
 const APP_ID = 'amzn1.ask.skill.c9178642-ece1-4ec4-886f-1bd55f313f46';
 
 const SKILL_NAME = 'mental support';
@@ -22,9 +17,7 @@ const HELP_MESSAGE = 'You can tell me if you want mental support, or, you can sa
 const HELP_REPROMPT = 'I want to help you. What can I do to help?';
 const STOP_MESSAGE = 'Goodbye! I hope you’ll keep talking to me about your thoughts when you are not happy. I love you';
 
-//=========================================================================================================================================
-//TODO: Replace this data with your own.  You can find translations of this data at http://github.com/alexa/skill-sample-node-js-fact/data
-//=========================================================================================================================================
+========================================================================================================================================
 const data = [
     'I’m here for you. You are not alone',
     'Please, you are important to me',
@@ -50,9 +43,7 @@ const data3 = [
     'Of course! I know some really good songs. Enjoy.',
     'Okay! Let me see. Ah I found one, here you go'
 ];
-//=========================================================================================================================================
-//Editing anything below this line might break your skill.
-//=========================================================================================================================================
+/=========================================================================================================================================
 
 const handlers = {
     'LaunchRequest': function () {
